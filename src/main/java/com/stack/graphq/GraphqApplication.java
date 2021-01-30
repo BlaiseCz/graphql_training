53cff09107ffcf97e21b4fe70fa029c134543c33
BlaiseCz <blazej.czekala@gmail.com>
2021-01-30 21:50:08 +0100

    gitignore update

---
+++ src/main/java/com/stack/graphq/GraphqApplication.java
@@ -0,0 +1,13 @@
+package com.stack.graphq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stack/graphq/GraphqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stack.graphq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
